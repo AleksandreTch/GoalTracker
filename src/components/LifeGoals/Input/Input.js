918b76657de39b23cad1b861495dedd019e951e5
Aleksandre Tcharbadze <riseofkelevra@gmail.com>
2022-11-29 23:28:31 -0500

    Added invalid input dynamix styling functionality

--- src/components/LifeGoals/Input/Input.js
+++ src/components/LifeGoals/Input/Input.js
@@ -5,23 +5,39 @@ import './Input.css';
 
 const Input = props => {
   const [enteredValue, setEnteredValue] = useState('');
+  const [isValid, setIsValid] = useState(true);
+  
 
   const goalInputChangeHandler = event => {
+    if(event.target.value.trim().length > 0){
+      setIsValid(true);
+    }
     setEnteredValue(event.target.value);
   };
 
+
+  
   const formSubmitHandler = event => {
     event.preventDefault();
+    if (enteredValue.trim().length === 0){
+      setIsValid(false);
+      return;
+    }
     props.onAddGoal(enteredValue);
   };
 
   return (
     <form onSubmit={formSubmitHandler}>
       <div className="form-control">
-        <label>Input Your Goal</label>
-        <input type="text" onChange={goalInputChangeHandler} />
+        <label style={{color: !isValid ? 'red' : 'black'}}>Input Your Goal</label>
+        <input 
+        style={{
+          border: !isValid ? 'solid 2px red' : 'solid 1px white',
+          background: !isValid ? 'transparent' : 'white'}} 
+        type="text" 
+        onChange={goalInputChangeHandler} />
       </div>
-      <Button type="submit">Add</Button>
+      <Button type="submit">Add Goal</Button>
     </form>
   );
 };
